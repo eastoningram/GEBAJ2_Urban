6fec9dcc22090c9ce48b1fc51200aca098eb4c8b
eastoningram <eastoningram@gmail.com>
2025-09-27 23:15:11 -0400

    copy flaskr

---
+++ backend/algorithm/flaskr/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return "A simple urban planning website, made for HackUMBC 2025!"
+
+@app.route('/team')
+def team():
+    return "Alex, Griffin, Easton, Ben, Josh^2."
+
+@app.route("/generateCity", methods=["POST"])
+def generateCity(): 
+    data = request.get_json()
+    population = data["population"]
+    area = data["area"]
+    print(population)
+    print(area)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
